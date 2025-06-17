8cb57aa9074a58f806bf4ed560609394822a9ade
ajitsagale <ajitsagale@gmail.com>
2025-06-17 14:22:16 +0530

    logo on login

--- src/components/Header1.tsx
+++ src/components/Header1.tsx
@@ -16,7 +16,7 @@ import ProfileMenu from './ProfileMenu'; // ProfileMenu component
 import { Link } from 'react-router-dom';
 
 const schoolName = 'Smartkidz Wakad';
-const logoUrl = '/images/happy_kids1.png'; // Corrected and simplified
+const logoUrl = '/images/smartkidz_logo.png'; // Corrected and simplified
 
 const Header1: React.FC = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
