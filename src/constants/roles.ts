3d0b1ce4b8efa630e8eb65d0cde99c1807b30a03
ajitsagale <ajitsagale@gmail.com>
2025-06-24 12:58:32 +0530

    remove dashboard

--- src/constants/roles.ts
+++ src/constants/roles.ts
@@ -8,9 +8,9 @@ export const Roles = {
 export const Screens = [
   { path: '/dashboard', label: 'Dashboard',roles: [Roles.SUPER_ADMIN] },
   { path: '/register-preschool', label: 'Register Pre-School',roles: [Roles.SUPER_ADMIN] },
-  { path: '/admin/dashboardAdmin', label: 'dashboardAdmin',roles: [Roles.ADMIN] },
-  { path: '/teacher/dashboardTeacher', label: 'dashboardTeacher',roles: [Roles.TEACHER] },
-  { path: '/parent/dashboardParent', label: 'dashboardParent',roles: [Roles.PARENT] },
+  // { path: '/admin/dashboardAdmin', label: 'dashboardAdmin',roles: [Roles.ADMIN] },
+  // { path: '/teacher/dashboardTeacher', label: 'dashboardTeacher',roles: [Roles.TEACHER] },
+  // { path: '/parent/dashboardParent', label: 'dashboardParent',roles: [Roles.PARENT] },
   { path: '/admin/users', label: 'User Management', roles: [Roles.ADMIN] },
   { path: '/teacher/homework', label: 'Homework', roles: [Roles.TEACHER] },
   { path: '/parent/homework', label: 'Homework', roles: [Roles.PARENT] },
