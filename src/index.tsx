91e2c47c9622f075dfc642d9503099641a86cc94
ajitsagale <ajitsagale@gmail.com>
2025-09-07 18:52:38 +0530

    ionic changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { CustomThemeProvider } from './contexts/ThemeContext';
+import { Capacitor } from '@capacitor/core';
+import { IonApp } from '@ionic/react';
+import { initMobile } from './mobile';
 
-ReactDOM.render(
+const isNative = Capacitor.isNativePlatform();
+
+if (isNative) {
+  initMobile();
+}
+
+const Root = () => (
   <React.StrictMode>
     <CustomThemeProvider>
-      <App />
+      {isNative ? (
+        <IonApp>
+          <App />
+        </IonApp>
+      ) : (
+        <App />
+      )}
     </CustomThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
+);
+
+ReactDOM.render(
+  <Root />,
   document.getElementById('root')
 );
 
